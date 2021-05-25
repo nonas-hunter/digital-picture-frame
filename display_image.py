02a702ef0b5d2d3bc49cbccf36a31c621ca161d0
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-05-25 01:41:41 -0400

    Display image in tkinter window

--- display_image.py
+++ display_image.py
@@ -1,4 +1,16 @@
-from PIL import Image
+from PIL import Image, ImageTk
+import tkinter
 
+# Define tkinter window
+root = tkinter.Tk()
+width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+
+# Open image using Pillow
 with Image.open("test_images/pattern.jpg") as image:
-    image.show()
+    tkinter_image = ImageTk.PhotoImage(image)
+
+# Create canvas and add image
+canvas = tkinter.Canvas(root, width=width, height=height)
+canvas.pack()
+canvas.create_image(width/2, height/2, image=tkinter_image, anchor=tkinter.CENTER)
+root.mainloop()
